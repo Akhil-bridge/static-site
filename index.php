5f0f897ad015760f4a3a82329d5f86b0334b1e32
Akhil Bridge <34673229+Akhil-bridge@users.noreply.github.com>
2019-09-01 16:34:01 +0530

    weqw
    eqwe

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
 
+<h1><?php echo "hellooo";?></h1>
+<p>This is a paragraph.</p>
 
-$assss= "";
-
-function hh()            {
-
-//testasd
-  return null;
-}
+</body>
+</html>
